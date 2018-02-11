6b7ccb0faacf6848d60b1a03ad8cee2ce0120414
N0ric <ItsDaGeek@users.noreply.github.com>
2018-02-11 12:05:11 -0800

    added monitoring configuration script

---
+++ ds/exporter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://dageek.tech/ds/node_exporter
+cp node_exporter /usr/local/bin/node_exporter
+rm node_exporter
+wget https://dageek.tech/ds/node_exporter.service
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+rm node_exporter.service
+echo "DONE"
