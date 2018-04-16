72ba6f54918a7735d86adefaf40a9d639c0afc11
n0ric <james.ca@me.com>
2018-04-15 19:35:39 -0700

    Added Weather and made rotating gradients

--- cds/app.js
+++ cds/app.js
@@ -5,7 +5,8 @@
 
     function startTime() {
         var todayTime = new Date().toLocaleTimeString('en-US', { hour12: true, hour: "numeric", minute: "numeric" });
-        document.getElementById('currentTime').innerHTML = todayTime;
+        var todayNumericalTime = todayTime.split(/(\s+)/)[0];
+        document.getElementById('currentTime').innerHTML = todayNumericalTime;
         t = setTimeout(function() {
             startTime()
         }, 1000);
@@ -17,3 +18,11 @@
     }
     startTime();
 })();
+
+function weather() {
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?id=5391811&APPID=f9668861219ddb735be050fa92d5616d").then(json => {
+        document.getElementById('currentWeather').innerHTML = "SD: " + json.weather[0].main;
+    })
+};
+weather()
+setInterval(weather, 600000000)
--- cds/background.js
+++ cds/background.js
@@ -1,8 +1,18 @@
+var colors = [
+    { r: "104", g: "227", b: "221" }, //Cyan
+    { r: "0", g: "137", b: "208" }, //Blue
+    { r: "146", g: "39", b: "143" }, //Purple
+    { r: "237", g: "28", b: "36" }, //Red
+    { r: "244", g: "121", b: "32" } //Orange
+];
+
+
 function background() {
     $.getJSON("https://raw.githubusercontent.com/dconnolly/chromecast-backgrounds/master/backgrounds.json").then(json => {
         var item = json[Math.floor(Math.random() * json.length)];
         var bg = item["url"]
-        document.body.style.backgroundImage = '-webkit-linear-gradient(bottom, rgb(0,0,0,.9) , transparent), url("' + bg + '")';
+        var color = colors[Math.floor(Math.random() * colors.length)]
+        document.body.style.backgroundImage = '-webkit-linear-gradient(303deg, rgb(' + color["r"] + ',' + color["g"] + ',' + color["b"] + ',0.9), transparent, rgb(0,0,0,.9)), url("' + bg + '")';
     });
 }
 
