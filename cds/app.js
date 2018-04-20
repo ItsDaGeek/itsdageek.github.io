ca1ab1e8822b7153f0c8390ecd26891f8f124b25
James Click <jrc@localhost.localdomain>
2018-04-20 09:01:45 -0700

    am I sane?
    no.

--- cds/app.js
+++ cds/app.js
@@ -19,6 +19,12 @@
     startTime();
 })();
 
+function getTime() {
+    var todayTime = new Date().toLocaleTimeString('en-US', { hour12: true, hour: "numeric", minute: "numeric" });
+    var todayNumericalTime = todayTime.split(/(\s+)/)[0];
+    return todayNumericalTime;
+}
+
 function weather() {
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?id=5391811&APPID=f9668861219ddb735be050fa92d5616d").then(json => {
         document.getElementById('currentWeather').innerHTML = "SD: " + json.weather[0].main;
@@ -26,3 +32,11 @@ function weather() {
 };
 weather()
 setInterval(weather, 600000000)
+
+var typed = new Typed('#secondaryInfo', {
+    strings: ["Welcome to Cymer Digital Studio", "Remember to Sign in at the front", "Have you seen our upcoming classes?", "Youth Institute applications are soon to be available!", "Does anyone read these?", "Made with Coffee by @realJamesClick"],
+    typeSpeed: 30,
+    showCursor: false,
+    loop: true,
+    backDelay: 15000,
+  });
--- cds/background.js
+++ cds/background.js
@@ -1,17 +1,7 @@
-var colors = [
-    { r: "104", g: "227", b: "221" }, //Cyan
-    { r: "0", g: "137", b: "208" }, //Blue
-    { r: "146", g: "39", b: "143" }, //Purple
-    { r: "237", g: "28", b: "36" }, //Red
-    { r: "244", g: "121", b: "32" } //Orange
-];
-
-
 function background() {
     $.getJSON("https://raw.githubusercontent.com/dconnolly/chromecast-backgrounds/master/backgrounds.json").then(json => {
         var item = json[Math.floor(Math.random() * json.length)];
         var bg = item["url"]
-        var color = colors[Math.floor(Math.random() * colors.length)]
         document.body.style.backgroundImage = 'url("' + bg + '")';
     });
 }
